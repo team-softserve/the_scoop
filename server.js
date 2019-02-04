8ee862207f825cd585e3fbc2ea062ee860e162bd
JeiWolfei <jvampy22@gmail.com>
2019-02-04 10:50:01 -0800

    intial commit and framework

---
+++ server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+require('./lib/utils/connect')();
+
+const app = require('./lib/app');
+
+app.listen(7890, () => {
+  // eslint-disable-next-line
+  console.log('Server started on port 7890');
+});

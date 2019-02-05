530d4eb94e25f9fc6a1147b22533f15001420967
JeiWolfei <jvampy22@gmail.com>
2019-02-05 10:24:23 -0800

    enprogress of logs

--- test/logs.test.js
+++ test/logs.test.js
@@ -1,29 +1,54 @@
+require('dotenv').config();
+require('../lib/utils/connect')();
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
 const Log = require('../../lib/models/Log');
-const { getErrors } = require('../util/helpers');
 const { Types } = require('mongoose');
 
-describe('log model', () => {
-  it('validates a good model', () => {
-    const data = {
+const createLog = (log) => {
+  return request(app)
+    .post('/logs')
+    .send({ 
       place_id: 'ChIJIfBAsjeuEmsRdgu9Pl1Ps48', 
       user: Types.ObjectId(),
-      rating: 'liked',
+      rating: '3',
       tags: ['dairy-free', 'kid friendly'],
       price: 2
-    };
+    })
+    .then(res => res.body);
+};
 
-    const log = new Log(data);
-    const jsonLog = log.toJSON();
-    expect(jsonLog).toEqual({ ...data, _id: expect.any(Object) });
+describe('logs', () => {
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
   });
 
-  it('fails when no required values are given', () => {
-    const log = new Log({});
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+  
+  it('validates a good model', () => {
+    return User.create({
+      username: 'test',
+      password: '234'
+    })
+      .then(createdUser => {
+        return Post.create({
+          user: createdUser._id.toString(),
 
-    const errors = getErrors(log.validateSync(), 5);
-    expect(errors.place_id.properties.message).toEqual('place_id required');
-    expect(errors.user.properties.message).toEqual('user required');
-    expect(errors.tags.properties.message).toEqual('at least one tag required');
-    expect(errors.price.properties.message).toEqual('price required 0-4');
+  
+         })
+          .then(post => expect(post.toJSON()).toEqual({
+            user: expect.any(Object),
+            price: '2', 
+            rating: '3',
+            tags: [],
+            _id: expect.any(Object)
+          }));
+      });
   });
 });
+ 

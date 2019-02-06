2b2edb9e29fbe720ae559c228e5d047af95d516b
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-05 16:25:28 -0800

    Working on Log Model test

---
+++ test/logs.test.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+require('../lib/utils/connect')();
+const request = require('supertest');
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+const Log = require('../lib/models/Log');
+const { Types } = require('mongoose');
+
+describe.only('Log Tests', () => {
+
+  beforeEach(done => {
+    return mongoose.connection.dropDatabase(() => {
+      done();
+    });
+  });
+
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
+  it('validates a good model', () => {
+    const log = new Log({ 
+      place_id: '1234', 
+      name: 'Portland Creamery', 
+      user: Types.ObjectId(), 
+      rating: { price: 4,  vibe: 4,  flavor: 5  }, 
+      tags: ['organic', 'dairy-free'], 
+      price: 3 });
+      console.log('log model', log);
+    expect(log.toJSON()).toEqual({
+      place_id: '1234', 
+      name: 'Portland Creamery', 
+      user: Types.ObjectId(), 
+      rating: { price: 4, vibe: 4, flavor: 5 }, 
+      tags: ['organic', 'dairy-free'], 
+      price: 3,
+      _id: expect.any(Types.ObjectId) 
+    });
+  });
+});

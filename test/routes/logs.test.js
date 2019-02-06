29d21712efee41701e99827d4f99934015a35549
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-06 11:10:40 -0800

    Added GET all/by id tests

--- test/routes/logs.test.js
+++ test/routes/logs.test.js
@@ -1,8 +1,8 @@
-//add mock data const { getUsers, getToken }
+//add mock data const { getUsers, getToken, getLogs }
 const request = require('supertest');
 const app = require('../../lib/app');
 
-describe('logs', () => {
+describe('Logs tests', () => {
   it('creates a log', () => {
     const createdUsers = getUsers();
     return request(app)
@@ -22,11 +22,44 @@ describe('logs', () => {
       });
   });
   it('gets all logs', () => {
-    
+    return request(app)
+      .get('/logs')
+      .then(res => {
+        return Promise.all([
+          Promise.resolve(res.body),
+          getLogs()
+        ]);
+      })
+      .then(([body, logs]) => {
+        expect(body).toHaveLength(logs.length);
+      });
   });
   
   it('gets a log by id', () => {
-
+    const createdUsers = getUsers();
+    return getLog()
+      .then(log => {
+        console.log('Log', log);
+        return Promise.all([
+          Promise.resolve(log),
+          request(app)
+            .get(`/logs/${log._id}`)
+            .set('Authorization', `Bearer ${getToken()}`)
+        ]);
+      })
+      /* eslint-disable-next-line */
+      .then(([log, res]) => {
+        console.log('res', res.body);
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          place_id: expect.any(String),
+          name: expect.any(String),
+          user: createdUsers[0]._id.toString(),
+          rating: expect.any(Number),
+          tags: expect.any(String),
+          price: expect.any(Number)
+        });
+      });
   });
 
 });

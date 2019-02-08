0cbecd1a6252c836549213941b544a53c03f73e5
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-07 16:32:54 -0800

    Added a Procfile.

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 require('./lib/utils/connect')();
 
 const app = require('./lib/app');
+const PORT = process.env.PORT || 7899;
 
-app.listen(7899, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line
-  console.log('Server started on port 7899');
+  console.log('Server started on port ', PORT);
 });
